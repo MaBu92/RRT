7b11a0d496b10cff2ab5c4cab4bca1222a3c4827
MaBu92 <marvin.m.busch@gmail.com>
2022-11-07 21:47:31 +0100

    rrt

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 //
 #include <chrono>
 
-#include "rrt.h"
 #include "rrt_star.h"
 
 const ConfigXY kStart(2, 2);
@@ -35,36 +34,37 @@ struct Timer {
 int main() {
     Timer timer;
 
-    srand(3);
+    //srand(3);
 
-    int n = 1000;
+    int n = 70;
 
-//    const ConfigSpace<ConfigXY> config_space(kMin, kMax);
-//    RRT<ConfigXY> rrt(kStart, kGoal, config_space);
-//    Path<Node<ConfigXY>> path = rrt.run(n);
+    const ConfigSpace<ConfigXY> config_space(kMin, kMax);
+    RRT<ConfigXY> rrt(kStart, kGoal, config_space);
+    Path<Node<ConfigXY>> path = rrt.run(n);
+    doSomething(rrt.nodes[0]);
 //    for (Node<ConfigXY> &node: path) {
 //        node.printConfig();
 //    }
 
 
 
-    const ConfigSpace<ConfigXYYaw> config_space2(kMin2, kMax2);
-    RRT<ConfigXYYaw> rrt2(kStart2, kGoal2, config_space2);
-    Path<Node<ConfigXYYaw>> path2 = rrt2.run(n);
+//    const ConfigSpace<ConfigXYYaw> config_space2(kMin2, kMax2);
+//    RRT<ConfigXYYaw> rrt2(kStart2, kGoal2, config_space2);
+//    Path<Node<ConfigXYYaw>> path2 = rrt2.run(n);
 //    for (Node<ConfigXYYaw> &node: path2) {
 //        node.printConfig();
 //    }
 
 
-    RRT_Star<ConfigXYYaw> rrt3(kStart2, kGoal2, config_space2);
-    Path<Node<ConfigXYYaw>> path3 = rrt3.run(n);
+//    RRT_Star<ConfigXYYaw> rrt3(kStart2, kGoal2, config_space2);
+//    Path<Node<ConfigXYYaw>> path3 = rrt3.run(n);
 //    for (Node<ConfigXYYaw> &node: path3) {
 //        node.printConfig();
 //    }
 
-    Eigen::Array<double, 3, 1> weight(1, 1, 0);
-    double min_cost = sqrt(((kGoal2 - kStart2)*weight).pow(2).sum());
-//    std::cout << path.back().cost / min_cost * 100 << std::endl;
-    std::cout << path2.back().getCost() / min_cost * 100 << std::endl;
-    std::cout << path3.back().getCost() / min_cost * 100 << std::endl;
+//    Eigen::Array<double, 3, 1> weight(1, 1, 0);
+//    double min_cost = sqrt(((kGoal2 - kStart2)*weight).pow(2).sum());
+////    std::cout << path.back().cost / min_cost * 100 << std::endl;
+//    std::cout << path2.back().getCost() / min_cost * 100 << std::endl;
+//    std::cout << path3.back().getCost() / min_cost * 100 << std::endl;
 }
--- rrt.cpp
+++ rrt.cpp
@@ -3,22 +3,29 @@
 //
 
 #include "ros/ros.h"
-#include "<visualization_msgs/Marker.h>"
-#include "rrt.h"
+#include <visualization_msgs/Marker.h>
+#include <rrt/rrt_star.h>
 
 using RosMarker = visualization_msgs::Marker;
 
-ConfigXY kStart(2, 2, 0);
-ConfigXY kGoal(18, 18, 90);
+ConfigXY kStart(2, 2);
+ConfigXY kGoal(18, 18);
 ConfigSpace<ConfigXY> kConfigSpace(0, 20, 0, 20);
 
-RosMarker createRosMarker(uint32_t shape, ConfigXY &kStart, std::array<double, 3> color);
+const double kGoalSampleRate = 0.5;
+const double kExtensionStepSize = 1;
+const double kPathResolution = 0.05;
+const double kNeighborhoodRadius = 3;
+
+RosMarker createRosMarker(uint32_t shape, ConfigXY kStart, std::array<double, 3> color);
 RosMarker createRosPoints(std::array<double, 3> color);
 inline void addPoint(RosMarker &points, ConfigXY &config);
 RosMarker createRosLineList(std::array<double, 3> color);
 inline void addLine(RosMarker &line_list, ConfigXY &start, ConfigXY &goal);
+RosMarker createRosLineStrip(std::array<double, 3> color);
 void addTreeEdgesToMarker(RosMarker &line_list, Node<ConfigXY> &node);
-void addFinalPath(RosMarker &line_list, Node<ConfigXY> &node);
+void addFinalPath(RosMarker &line_list, Path<ConfigXY> &node);
+
 
 int main(int argc, char **argv)
 {
@@ -27,18 +34,17 @@ int main(int argc, char **argv)
     ros::Publisher rrt_pub = n.advertise<visualization_msgs::Marker>("rrt_publisher", 1);
     ros::Rate loop_rate(10);
 
-    RRT<ConfigXY> rrt(kStart, kGoal, kConfigSpace);
-    rrt.run(100);
+    RRT_Star<ConfigXY> rrt(kConfigSpace, kExtensionStepSize, kGoalSampleRate, kPathResolution, kNeighborhoodRadius);
+    Path<ConfigXY> path = rrt.run(kStart, kGoal, 30);
 
-    RosMarker start = createRosMarker(RosMarker::POINTS, kStart, {1, 0, 0});
-    RosMarker goal = createRosMarker(RosMarker::POINTS, kGoal, {0, 1, 0});
+    RosMarker start = createRosMarker(RosMarker::CUBE, ConfigXY(3.75, 5.52), {1, 0, 0});
+    RosMarker goal = createRosMarker(RosMarker::CUBE, ConfigXY(4.29, 4.68), {0, 1, 0});
 
     RosMarker edges = createRosLineList({0, 0, 1});
-    addTreeEdgesToMarker(edges, &rrt.nodes[0]);
+    addTreeEdgesToMarker(edges, rrt.getStartNode());
 
-    RosMarker final_path = createRosLineList({0, 1, 0});
-    addFinalPath(final_path, &rrt.nodes.back());
-
+    //RosMarker final_path = createRosLineStrip({0, 1, 0});
+    //addFinalPath(final_path, path);
 
     int count = 0;
     while (ros::ok())
@@ -53,7 +59,7 @@ int main(int argc, char **argv)
         rrt_pub.publish(start);
         rrt_pub.publish(goal);
         rrt_pub.publish(edges);
-        rrt_pub.publish(final_path);
+        //rrt_pub.publish(final_path);
 
         loop_rate.sleep();
     }
@@ -66,7 +72,7 @@ int main(int argc, char **argv)
 RosMarker createRosMarker(uint32_t shape, ConfigXY config, std::array<double, 3> color) {
     static int marker_id = 0;
     RosMarker marker;
-    marker.header.frame_id = "/rrt";
+    marker.header.frame_id = "rrt";
     marker.header.stamp = ros::Time::now();
 
     marker.ns = "Start/Goal";
@@ -82,6 +88,7 @@ RosMarker createRosMarker(uint32_t shape, ConfigXY config, std::array<double, 3>
 
     marker.scale.x = 0.2;
     marker.scale.y = 0.2;
+    marker.scale.z = 0.2;
 
     marker.color.r = color[0];
     marker.color.g = color[1];
@@ -133,7 +140,7 @@ inline void addPoint(RosMarker &marker, ConfigXY &config) {
 RosMarker createRosLineList(std::array<double, 3> color) {
     static int line_list_id = 0;
     RosMarker line_list;
-    line_list.header.frame_id = "/rrt";
+    line_list.header.frame_id = "rrt";
     line_list.header.stamp = ros::Time::now();
 
     line_list.ns = "Edges";
@@ -144,7 +151,7 @@ RosMarker createRosLineList(std::array<double, 3> color) {
 
     line_list.pose.orientation.w = 1.0;
 
-    line_list.scale.x = 0.1;
+    line_list.scale.x = 0.01;
 
     line_list.color.r = color[0];
     line_list.color.g = color[1];
@@ -154,6 +161,7 @@ RosMarker createRosLineList(std::array<double, 3> color) {
     line_list.lifetime = ros::Duration();
 
     line_list_id++;
+
     return line_list;
 }
 
@@ -170,7 +178,7 @@ inline void addLine(RosMarker &line_list, ConfigXY &start, ConfigXY &goal) {
 RosMarker createRosLineStrip(std::array<double, 3> color) {
     static int line_strip_id = 0;
     RosMarker line_strip;
-    line_strip.header.frame_id = "/rrt";
+    line_strip.header.frame_id = "rrt";
     line_strip.header.stamp = ros::Time::now();
 
     line_strip.ns = "FinalPath";
@@ -181,7 +189,7 @@ RosMarker createRosLineStrip(std::array<double, 3> color) {
 
     line_strip.pose.orientation.w = 1.0;
 
-    line_strip.scale.x = 0.1;
+    line_strip.scale.x = 0.03;
 
     line_strip.color.r = color[0];
     line_strip.color.g = color[1];
@@ -194,15 +202,15 @@ RosMarker createRosLineStrip(std::array<double, 3> color) {
     return line_strip;
 }
 
-void addTreeEdgesToMarker(RosMarker &line_list, Node<ConfigXY> *node) {
-    for (Node<Config> *child: node->getChildren()) {
-        addLine(line_list, node->config, child->config);
-        addTreeEdgesToMarker(line_list, child);
+void addTreeEdgesToMarker(RosMarker &line_list, Node<ConfigXY> &node) {
+    for (Node<ConfigXY> *child: node.getChildren()) {
+        addLine(line_list, node.getConfig(), child->getConfig());
+        addTreeEdgesToMarker(line_list, *child);
     }
 }
 
-void addFinalPath(RosMarker &line_strip, Node<ConfigXY> *node) {
-    if (!node) return;
-    addPoint(line_strip, node->config);
-    addFinalPath(line_strip, node.getParent());
+void addFinalPath(RosMarker &line_strip, Path<ConfigXY> &path) {
+    for (ConfigXY &config: path) {
+        addPoint(line_strip, config);
+    }
 }
